0b6e80b90b9f7db281d8f83814e15ea922bd8644
Suntek Oh <suntekoh@outlook.com>
2022-08-26 13:10:45 -0400

    identity schema changed

---
+++ Common/Models/Identity/ApplicationRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace Common.Models.Identity;
+
+public class ApplicationRole : IdentityRole<Guid>
+{
+    public virtual ICollection<ApplicationUserRole> UserRoles { get; set; }
+    public virtual ICollection<ApplicationRoleClaim> RoleClaims { get; set; }
+}
---
+++ Common/Models/Identity/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Models.Identity;
+
+public class ApplicationUser : IdentityUser<Guid>
+{
+    public virtual ICollection<ApplicationUserClaim> Claims { get; set; }
+    public virtual ICollection<ApplicationUserLogin> Logins { get; set; }
+    public virtual ICollection<ApplicationUserToken> Tokens { get; set; }
+    public virtual ICollection<ApplicationUserRole> UserRoles { get; set; }
+}
