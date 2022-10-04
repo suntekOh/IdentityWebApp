6eded705847158d949d800d7b8c8d735470879ce
Suntek Oh <suntekoh@outlook.com>
2022-10-04 15:18:55 -0400

    responsive web design code for menu added

--- IdentityPortal/wwwroot/js/site.js
+++ IdentityPortal/wwwroot/js/site.js
@@ -1,9 +1,18 @@
-﻿var site = {};
+﻿var site = {
+    showed: false
+};
 
 site.initialize = function () {
-    $(document).on('click', "main .dropdown-menu a", function (e) {
-        console.log("1")
-        e.preventDefault();
-    });
+    $(document).on('click', "main #navbarDropdownMenuLink", function (e) {
+        var dropdownMenuPointer = $(this).siblings(".dropdown-menu");
+        if (site.showed) {
+            dropdownMenuPointer.removeClass('stay-open')
+            dropdownMenuPointer.addClass('d-none')
+        } else {
+            dropdownMenuPointer.removeClass('d-none')
+            dropdownMenuPointer.addClass('stay-open')
+        }
 
+        site.showed = !(site.showed);
+    });
 }
